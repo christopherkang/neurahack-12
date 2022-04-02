c07590bc9c6344ac0107e0fdf52ef78238bb1bc3
Christopher Kang <42285636+christopherkang@users.noreply.github.com>
2022-04-02 14:00:13 -0700

    datas

--- hello-world/test-denoiseTrans.py
+++ hello-world/test-denoiseTrans.py
@@ -10,7 +10,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations, WindowFunctions
 
 
 def main():
@@ -19,18 +19,20 @@ def main():
     # use synthetic board for demo
     params = BrainFlowInputParams()
     # board_id = BoardIds.SYNTHETIC_BOARD.value
+    
+    params = BrainFlowInputParams()
     board_id = 38
-    params.serial_number = "Muse-748A"
-    # params.serial_number = "Muse-6D79"
-
+    params.serial_number = "Muse-6D79"
+    sampling_rate = BoardShim.get_sampling_rate(board_id)
     board = BoardShim(board_id, params)
     board.prepare_session()
     board.start_stream()
     BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
-    time.sleep(20)
-    data = board.get_board_data()
+    time.sleep(10)
+    data = board.get_current_board_data(DataFilter.get_nearest_power_of_two(sampling_rate))
     board.stop_stream()
     board.release_session()
+
 
     # demo how to convert it to pandas DF and plot data
     eeg_channels = BoardShim.get_eeg_channels(board_id)
@@ -61,11 +63,33 @@ def main():
             DataFilter.perform_wavelet_denoising(data[channel], 'coif3', 3)
 
     df = pd.DataFrame(np.transpose(data))
-    df.to_pickle("denoised_pickle")
-    plt.figure()
-    df[eeg_channels].plot(subplots=True)
-    plt.savefig('after_processing.png')
+    df.to_pickle("jack_open_denoise")
+    
 
+    for count, channel in enumerate(eeg_channels):
+        # print('Original data for channel %d:' % channel)
+        # print(data[channel])
+        # demo for wavelet transforms
+        # wavelet_coeffs format is[A(J) D(J) D(J-1) ..... D(1)] where J is decomposition level, A - app coeffs, D - detailed coeffs
+        # lengths array stores lengths for each block
+        wavelet_coeffs, lengths = DataFilter.perform_wavelet_transform(data[channel], 'db5', 3)
+        app_coefs = wavelet_coeffs[0: lengths[0]]
+        detailed_coeffs_first_block = wavelet_coeffs[lengths[0]: lengths[1]]
+        # you can do smth with wavelet coeffs here, for example denoising works via thresholds 
+        # for wavelets coefficients
+        restored_data = DataFilter.perform_inverse_wavelet_transform((wavelet_coeffs, lengths), data[channel].shape[0],'db5', 3)
+        # print('Restored data after wavelet transform for channel %d:' % channel)
+        # print(restored_data)
+
+        # demo for fft, len of data must be a power of 2
+        fft_data = DataFilter.perform_fft(data[channel], WindowFunctions.NO_WINDOW.value)
+        # len of fft_data is N / 2 + 1
+        restored_fft_data = DataFilter.perform_ifft(fft_data)
+        # print('Restored data after fft for channel %d:' % channel)
+        # print(restored_fft_data)
+
+    df = pd.DataFrame(np.transpose(data))
+    df.to_pickle("jack_open_trans")
 
 if __name__ == "__main__":
     main()
