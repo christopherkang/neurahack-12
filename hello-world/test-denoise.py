a13d1f07d9d787f73b6723dd0b7323c981385c6b
Christopher Kang <42285636+christopherkang@users.noreply.github.com>
2022-04-02 17:32:31 -0700

    save

--- hello-world/test-denoise.py
+++ hello-world/test-denoise.py
@@ -1,4 +1,5 @@
 import argparse
+from difflib import restore
 import time
 import brainflow
 import numpy as np
@@ -20,24 +21,20 @@ def main():
     params = BrainFlowInputParams()
     # board_id = BoardIds.SYNTHETIC_BOARD.value
     
-    params = BrainFlowInputParams()
     board_id = 38
     params.serial_number = "Muse-6D79"
-    sampling_rate = BoardShim.get_sampling_rate(board_id)
+
     board = BoardShim(board_id, params)
     board.prepare_session()
     board.start_stream()
     BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
-    time.sleep(10)
-    data = board.get_current_board_data(DataFilter.get_nearest_power_of_two(sampling_rate))
+    time.sleep(60)
+    data = board.get_board_data()
     board.stop_stream()
     board.release_session()
 
-
     # demo how to convert it to pandas DF and plot data
     eeg_channels = BoardShim.get_eeg_channels(board_id)
-    print(eeg_channels)
-    # assert 1 == 0
     df = pd.DataFrame(np.transpose(data))
     plt.figure()
     df[eeg_channels].plot(subplots=True)
@@ -63,33 +60,39 @@ def main():
             DataFilter.perform_wavelet_denoising(data[channel], 'coif3', 3)
 
     df = pd.DataFrame(np.transpose(data))
-    df.to_pickle("jack_open_denoise")
+    df.to_pickle("Chris_close_1")
+    print(data.shape)
     
+    # restored_fft = []
+    # for count, channel in enumerate(eeg_channels):
+    #     # print(f"{channel}!!!!!!!!!!!")
+    #     # print('Original data for channel %d:' % channel)
+    #     # print(len(data[channel]))
+    #     # demo for wavelet transforms
+    #     # wavelet_coeffs format is[A(J) D(J) D(J-1) ..... D(1)] where J is decomposition level, A - app coeffs, D - detailed coeffs
+    #     # lengths array stores lengths for each block
+    #     wavelet_coeffs, lengths = DataFilter.perform_wavelet_transform(data[channel], 'db5', 3)
+    #     app_coefs = wavelet_coeffs[0: lengths[0]]
+    #     detailed_coeffs_first_block = wavelet_coeffs[lengths[0]: lengths[1]]
+    #     # you can do smth with wavelet coeffs here, for example denoising works via thresholds 
+    #     # for wavelets coefficients
+    #     restored_data = DataFilter.perform_inverse_wavelet_transform((wavelet_coeffs, lengths), data[channel].shape[0],'db5', 3)
+    #     # print('Restored data after wavelet transform for channel %d:' % channel)
+    #     # print(len(restored_data))
 
-    for count, channel in enumerate(eeg_channels):
-        # print('Original data for channel %d:' % channel)
-        # print(data[channel])
-        # demo for wavelet transforms
-        # wavelet_coeffs format is[A(J) D(J) D(J-1) ..... D(1)] where J is decomposition level, A - app coeffs, D - detailed coeffs
-        # lengths array stores lengths for each block
-        wavelet_coeffs, lengths = DataFilter.perform_wavelet_transform(data[channel], 'db5', 3)
-        app_coefs = wavelet_coeffs[0: lengths[0]]
-        detailed_coeffs_first_block = wavelet_coeffs[lengths[0]: lengths[1]]
-        # you can do smth with wavelet coeffs here, for example denoising works via thresholds 
-        # for wavelets coefficients
-        restored_data = DataFilter.perform_inverse_wavelet_transform((wavelet_coeffs, lengths), data[channel].shape[0],'db5', 3)
-        # print('Restored data after wavelet transform for channel %d:' % channel)
-        # print(restored_data)
+    #     # demo for fft, len of data must be a power of 2
+    #     fft_data = DataFilter.perform_fft(data[channel], WindowFunctions.NO_WINDOW.value)
+    #     # len of fft_data is N / 2 + 1
+    #     restored_fft.append(fft_data)
+    #     # restored_fft.append(DataFilter.perform_ifft(fft_data))
+    #     # print('Restored data after fft for channel %d:' % channel)
+    #     # print(DataFilter.perform_ifft(fft_data))
+    
+    # restored_fft_data = np.array(restored_fft)
+    # print(restored_fft_data.shape)
 
-        # demo for fft, len of data must be a power of 2
-        fft_data = DataFilter.perform_fft(data[channel], WindowFunctions.NO_WINDOW.value)
-        # len of fft_data is N / 2 + 1
-        restored_fft_data = DataFilter.perform_ifft(fft_data)
-        # print('Restored data after fft for channel %d:' % channel)
-        # print(restored_fft_data)
-
-    df = pd.DataFrame(np.transpose(data))
-    df.to_pickle("jack_open_trans")
+    # df = pd.DataFrame(np.transpose(restored_fft_data))
+    # df.to_pickle("jack_test_trans_fft")
 
 if __name__ == "__main__":
     main()
